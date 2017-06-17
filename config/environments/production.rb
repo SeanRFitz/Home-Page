f4fe76ccacad33dc69e6d5c6ed75091877c4931e
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-06-16 19:16:34 -0500

    Removed a conflict between config.asset.compile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
   
   # To get the background image to load in Heroku
   config.serve_static_assets = true
-  config.assets.compile = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -31,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
